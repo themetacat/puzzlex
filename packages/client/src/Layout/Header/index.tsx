d00d879d5997e764fef51d1eca40e14592ffbe6d
mashen <undefined>
2024-11-08 01:55:53 +0800

    home

--- packages/client/src/Layout/Header/index.tsx
+++ packages/client/src/Layout/Header/index.tsx
@@ -1,22 +1,42 @@
+import { useNavigate } from 'react-router-dom';
 import $style from './index.module.scss';
 
 const Header = () => {
+
+    const history = useNavigate();
+
+    const onNavigate = (path: string) => {
+        history(path);
+    };
+
+    const onConnect = () => {};
+    const onClickIcon = (path: string) => {};
+
     return (
         <div className={$style.header}>
-            <div className={$style['header-left']}>
+            <div className={$style['header-left']} onClick={() => onNavigate('/')}>
                 <div className={$style['header-left-logo']}></div>
                 <div className={$style['header-left-text']}>PuzzleX</div>
             </div>
             <div className={$style['header-menu']}>
-                <div className={$style['header-menu-item']}>PuzzleX</div>
-                <div className={$style['header-menu-item']}>Create</div>
+                <div className={$style['header-menu-item']} onClick={() => onNavigate('/')}>PuzzleX</div>
+                <div className={$style['header-menu-item']} onClick={() => onNavigate('/create-game')}>Create</div>
             </div>
             <div className={$style['header-right']}>
-                <div className={`${$style['header-right-item']} ${$style['icon-x']}`}></div>
-                <div className={`${$style['header-right-item']} ${$style['icon-feishu']}`}></div>
-                <div className={`${$style['header-right-item']} ${$style['icon-git']}`}></div>
+                <div
+                    className={`${$style['header-right-item']} ${$style['icon-x']}`}
+                    onClick={() => onClickIcon('x')}
+                ></div>
+                <div
+                    className={`${$style['header-right-item']} ${$style['icon-feishu']}`}
+                    onClick={() => onClickIcon('feishu')}
+                ></div>
+                <div
+                    className={`${$style['header-right-item']} ${$style['icon-git']}`}
+                    onClick={() => onClickIcon('git')}
+                ></div>
             </div>
-            <div className={$style['header-connect']}>Connect</div>
+            <div className={$style['header-connect']} onClick={onConnect}>Connect</div>
         </div>
     )
 };
--- packages/client/src/Pages/Home/index.tsx
+++ packages/client/src/Pages/Home/index.tsx
@@ -1,6 +1,30 @@
+import {List} from 'antd';
+import VirtualList from 'rc-virtual-list';
+import GameCard from './components/GameCard';
+import $style from './index.module.scss';
+
 const Home = () => {
     return (
-        <div>HOME</div>
+        <div className={$style['home']}>
+            <div className={$style['home-top']}>
+                <div className={$style['home-top-title']}>
+                    Explore, Create, and Play
+                </div>
+                <div className={$style['home-top-desc']}>
+                    In the World of Crypto Puzzles.
+                </div>
+            </div>
+            <div className={$style['home-main']}>
+                <div className={$style['home-main-title']}>Puzzles</div>
+                <div className={$style['home-main-content']}>
+                    <GameCard />
+                    <GameCard />
+                    <GameCard />
+                    <GameCard />
+                    <GameCard />
+                </div>
+            </div>
+        </div>
     )
 };
 
