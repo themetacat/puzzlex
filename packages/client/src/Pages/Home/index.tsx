4bdf53d3d2ce485d6d80f2bb8b30c41467b7a0ad
mashen <undefined>
2024-11-08 22:22:40 +0800

    home

--- packages/client/src/Pages/Home/index.tsx
+++ packages/client/src/Pages/Home/index.tsx
@@ -1,9 +1,18 @@
-import {List} from 'antd';
-import VirtualList from 'rc-virtual-list';
+import { List } from 'antd';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import GameCard from './components/GameCard';
 import $style from './index.module.scss';
 
+const listGrid = {
+    column: 2,
+    gutter: 30
+};
+
 const Home = () => {
+    const gameList = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }, { id: 7 }];
+
+    const onNext = () => {};
+
     return (
         <div className={$style['home']}>
             <div className={$style['home-top']}>
@@ -16,12 +25,37 @@ const Home = () => {
             </div>
             <div className={$style['home-main']}>
                 <div className={$style['home-main-title']}>Puzzles</div>
-                <div className={$style['home-main-content']}>
-                    <GameCard />
-                    <GameCard />
-                    <GameCard />
-                    <GameCard />
-                    <GameCard />
+                <div
+                    id="scrollableDiv"
+                    className={$style['home-main-list']}
+                >
+                    <InfiniteScroll
+                        style={{
+                            // overflowX: 'hidden'
+                        }}
+                        dataLength={gameList.length}
+                        hasMore={gameList.length < 4}
+                        loader={<div className={$style['loading']}></div>}
+                        next={() => onNext()}
+                        endMessage={<div className={$style['end-text']}>All Caught Up!</div>}
+                    >
+                    <List
+                        grid={listGrid}
+                        dataSource={gameList}
+                        itemLayout='horizontal'
+                        renderItem={(item: any) => {
+                            return (
+                                <List.Item key={item.id} style={{
+                                    marginBottom: '30px'
+                                }}>
+                                    <GameCard />
+                                </List.Item>
+                            )
+                        }}
+                    >
+                    </List>
+                    </InfiniteScroll>
+                   
                 </div>
             </div>
         </div>
